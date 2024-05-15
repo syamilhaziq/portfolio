92a6b37aaef5657359d9f0b5650fcbb4447da253
Syamilhaziq Shahimi <128269352+syamilhaziq@users.noreply.github.com>
2024-05-15 02:18:29 +0000

    Fixing the phone display | connecting form to  sheet | some adjustment to button

--- script.js
+++ script.js
@@ -11,3 +11,26 @@ function opentab(tabname){
   event.currentTarget.classList.add("active-link");
   document.getElementById(tabname).classList.add("active-tab");
 }
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+  sidemenu.style.right = "0";
+}
+function closemenu(){
+  sidemenu.style.right = "-200px";
+}
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzZ1V0KOK_rUWPQBB21zPN3vQAv3gQwM7yQ6BjPxN2eArLVbJbApTHrweqNYtmcVmyLRw/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {msg.innerHTML= "Message Submitted 😊" 
+      setTimeout(function(){msg.innerHTML=""},5000)
+      form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
+
